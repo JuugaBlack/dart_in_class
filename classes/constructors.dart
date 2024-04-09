43de6d83cabdd4eba96aa8767a59da80a19b7001
a a <1609354709@qq.com>
2024-04-09 09:27:40 +0000

    week7

--- classes/constructors.dart
+++ classes/constructors.dart
@@ -105,3 +105,44 @@ Point.withAssert(this.x, this.y) : assert(x >= 0) {
   print('In Point.withAssert(): ($x, $y)');
 }
 
+// 重定向构造函数
+class Point {
+  double x, y;
+  // The main constructor for this class.
+  Point(this.x, this.y);
+  // Delegates to the main constructor.
+  Point.alongXAxis(double x) : this(x, 0);
+}
+
+// 常量构造函数
+class ImmutablePoint {
+  static const ImmutablePoint origin = ImmutablePoint(0, 0);
+  final double x, y;
+  const ImmutablePoint(this.x, this.y);
+}
+
+// 工厂构造函数
+// 在实现并不总是创建其类的新实例的构造函数时，请使用关键字factory
+class Logger {
+  final String name;
+  bool mute = false;
+  // _cache is library-private, thanks to
+  // the _ in front of its name.
+  static final Map<String, Logger> _cache = <String, Logger>{};
+  factory Logger(String name) {
+    return _cache.putIfAbsent(name, () => Logger._internal(name));
+  }
+  factory Logger.fromJson(Map<String, Object> json) {
+    return Logger(json['name'].toString());
+  }
+  Logger._internal(this.name);
+  void log(String msg) {
+    if (!mute) print(msg);
+  }
+}
+// 工厂构造函数无权访问this
+var logger = Logger('UI');
+logger.log('Button clicked');
+
+var logMap = {'name': 'UI'};
+var loggerJson = Logger.fromJson(logMap);
